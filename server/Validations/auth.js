bde5ecf00f5b348fd02167a941a339abbf8f2f26
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-09-12 21:29:50 +0530

    Added : JOI Validations

---
+++ server/Validations/auth.js
@@ -0,0 +1,22 @@
+import joi from "joi";
+
+export const validateSignUp = (userData) => {
+  const Schema = joi.object({
+    username: joi.string().required().min(5),
+    fullname: joi.string().required().min(5),
+    email: joi.string().email().required(),
+    password: joi.string().min(5).required(),
+  });
+
+  return Schema.validateAsync(userData);
+};
+
+export const validateSignIn = (userData) => {
+  const Schema = joi.object({
+    email: joi.string().email(),
+    username: joi.string().min(5), 
+    password: joi.string().min(5).required(),
+  })
+  .or('email', 'username') 
+  return Schema.validateAsync(userData);
+};
